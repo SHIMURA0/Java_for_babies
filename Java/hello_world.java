5d4d82bf03eec60c30a92864d3049f3cbf53892d
Cyclotomic Fields <142958731+SHIMURA0@users.noreply.github.com>
2025-07-22 09:47:31 +0800

    Create hello_world.java

---
+++ Java/hello_world.java
@@ -0,0 +1,5 @@
+public class HelloWorld { // Define a public class named HelloWorld
+    public static void main(String[] args) { // Main method
+        System.out.println("Hello, World!"); // Print "Hello, World!" to the console
+    }
+}
